789254c95413f4d6975070846fe6c0290bbe16a7
Mike Ralphson <mike.ralphson@gmail.com>
2017-10-09 12:20:49 +0100

    Update clone.js

--- packages/reftools/clone.js
+++ packages/reftools/clone.js
@@ -7,13 +7,31 @@ function clone(obj) {
 function shallowClone(obj) {
     let result = {};
     for (let p in obj) {
-        result[p] = obj[p];
+        if (obj.hasOwnProperty(p)) {
+            result[p] = obj[p];
+        }
     }
     return result;
 }
 
+function deepClone(obj) {
+    let result = {};
+    for (let p in obj) {
+        if (obj.hasOwnProperty(p)) {
+            result[p] = (typeof obj[p] === 'object') ? deepClone(obj[p]) : obj[p];
+        }
+    }
+    return result;
+}
+
+function fastClone(obj) {
+    return Object.assign({},obj);
+}
+
 module.exports = {
     clone : clone,
-    shallowClone : shallowClone
+    shallowClone : shallowClone,
+    deepClone : deepClone,
+    fastClone : fastClone
 };
 
