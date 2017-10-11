cc81a4ebfaf479081c6324708f545965b149f5b6
Mike Ralphson <mike.ralphson@gmail.com>
2017-10-11 08:08:38 +0100

    Log utility function in dereference.js

--- packages/reftools/dereference.js
+++ packages/reftools/dereference.js
@@ -6,6 +6,24 @@ const recurse = require('./recurse.js').recurse;
 const clone = require('./clone.js').shallowClone;
 const jptr = require('jgexml/jpath.js');
 
+var getLogger = function (options) {
+    if (options && options.verbose) {
+        return {
+            warn: function() {
+                var args = Array.prototype.slice.call(arguments);
+                console.warn.apply(console, args);
+            }
+        }
+    }
+    else {
+        return {
+            warn: function() {
+                //nop
+            }
+        }
+    }
+}
+
 function dereference(o,definitions,options) {
     if (!options) options = {};
     if (!options.cache) options.cache = {};
@@ -14,6 +32,9 @@ function dereference(o,definitions,options) {
     let obj = (options.depth > 1 ? o : clone(o));
     let defs = (options.depth > 1 ? definitions : clone(definitions));
     if (!options.master) options.master = obj;
+
+    let logger = getLogger(options);
+
     let changes = 1;
     while (changes > 0) {
         changes = 0;
@@ -25,9 +46,7 @@ function dereference(o,definitions,options) {
                 entry.path = state.path.split('/$ref')[0];
                 entry.key = obj[key];
                 entry.org = obj[key];
-                if (options && options.verbose) {
-                    console.warn('Dereffing %s at %s',obj[key],entry.path);
-                }
+                logger.warn('Dereffing %s at %s',obj[key],entry.path);
                 entry.source = defs;
                 entry.data = jptr.jptr(entry.source,entry.key);
                 if (entry.data === false) {
@@ -41,17 +60,17 @@ function dereference(o,definitions,options) {
             else {
                 let entry = options.cache[obj[key]];
                 if (entry.resolved) {
-                    console.warn('Patching %s for %s',obj[key],entry.path);
+                    logger.warn('Patching %s for %s',obj[key],entry.path);
                     state.parent[state.pkey] = entry.data;
                 }
                 else if (obj[key] === entry.path) {
-                    console.warn('Tight circle at %s',entry.path);
-                    console.warn(util.inspect(options.cache));
+                    logger.warn('Tight circle at %s',entry.path);
+                    logger.warn(util.inspect(options.cache));
                     process.exit(1);
                 }
                 else {
-                    console.warn('Unresolved ref');
-                    console.warn(util.inspect(entry));
+                    logger.warn('Unresolved ref');
+                    logger.warn(util.inspect(entry));
                     state.parent[state.pkey] = jptr.jptr(defs,entry.path);
                     if (state.parent[state.pkey] === false) {
                         state.parent[state.pkey] = jptr.jptr(defs,entry.key);
