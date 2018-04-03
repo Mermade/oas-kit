f3a6f41a2229566fce537715549c68bc5ed85873
Mike Ralphson <mike.ralphson@gmail.com>
2018-04-03 17:21:02 +0100

    reftools; findObj and travis update

---
+++ packages/reftools/test/testfindobj.js
@@ -0,0 +1,34 @@
+const findObj = require('../lib/findObj.js').findObj;
+const should = require('should');
+
+const data = { a: { b: true } };
+
+describe('findObj',function(){
+    it('should not find missing object',function(){
+        findObj(data,null).found.should.be.false();
+    });
+    it('should find itself',function(){
+        findObj(data,data).found.should.be.true();
+    });
+    it('should find itself, path',function(){
+        findObj(data,data).path.should.be.exactly('#/');
+    });
+    it('should find itself, parent',function(){
+        should(findObj(data,data).parent).be.exactly(null);
+    });
+    it('should find child object',function(){
+        findObj(data,data.a).found.should.be.true();
+    });
+    it('should find child object, path',function(){
+        findObj(data,data.a).path.should.be.exactly('#/a');
+    });
+    it('should find child object, parent',function(){
+        findObj(data,data.a).parent.should.be.exactly(data);
+    });
+    it('should find grandchild object',function(){
+        findObj(data,data.a.b).found.should.be.true();
+    });
+    it('should not find similar object',function(){
+        findObj(data,{ b: true }).found.should.be.false();
+    });
+});
