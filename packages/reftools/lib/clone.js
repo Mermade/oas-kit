045a886bfed89dc65d86fddf7259ad1472a37a5f
Davide Bianchi <davide.bianchi@mia-platform.eu>
2020-02-09 23:46:51 +0100

    remove circular clone fn

--- packages/reftools/lib/clone.js
+++ packages/reftools/lib/clone.js
@@ -42,35 +42,6 @@ function fastClone(obj) {
     return Object.assign({},obj);
 }
 
-/**
-* Source: stackoverflow http://bit.ly/2A1Kha6
-*/
-
-// function circularClone(obj, hash) {
-//     if (!hash) hash = new WeakMap();
-//     // Do not try to clone primitives or functions
-//     if (Object(obj) !== obj || obj instanceof Function) return obj;
-//     if (hash.has(obj)) return hash.get(obj); // Cyclic reference
-//     try { // Try to run constructor (without arguments, as we don't know them)
-//         var result = new obj.constructor();
-//     } catch(e) { // Constructor failed, create object without running the constructor
-//         result = Object.create(Object.getPrototypeOf(obj));
-//     }
-//     // Optional: support for some standard constructors (extend as desired)
-//     /*if (obj instanceof Map)
-//         Array.from(obj, ([key, val]) => result.set(circularClone(key, hash),
-//                                                    circularClone(val, hash)) );
-//     else if (obj instanceof Set)
-//         Array.from(obj, (key) => result.add(circularClone(key, hash)) );
-//     */
-//     // Register in hash
-//     hash.set(obj, result);
-//     // Clone and assign enumerable own properties recursively
-//     return Object.assign(result, ...Object.keys(obj).map (
-//         key => ({ [key]: circularClone(obj[key], hash) }) ));
-// }
-
-
 module.exports = {
     nop : nop,
     clone : deepCopy,
