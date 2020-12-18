f6470023f9ad77c1cc5a2fe31542a8b057337920
Kothari, Mihir <MKothari@cvent.com>
2020-12-18 12:23:59 -0600

    API-3276 move tests

--- test/resolver.test.js
+++ test/resolver.test.js
@@ -14,6 +14,16 @@ const tests = fs.readdirSync(path.join(__dirname,'resolver')).filter(file => {
     return fs.statSync(path.join(__dirname, 'resolver', file)).isDirectory() && file !== 'include';
 });
 
+sinon.stub(crypto, 'createHash').callsFake(() => {
+    return {
+        update: sinon.stub().callsFake(() => {
+            return {
+                digest: sinon.stub().returns('123')
+            };
+        })
+    };
+});
+
 describe('Resolver tests', () => {
 tests.forEach((test) => {
     describe(test, () => {
@@ -25,6 +35,9 @@ tests.forEach((test) => {
             let options = { resolve: true, preserveMiro: false, source: inputSpec };
             try {
                 options = Object.assign({},options,yaml.parse(fs.readFileSync(path.join(__dirname, 'resolver', test, 'options.yaml'),'utf8'),{schema:'core'}));
+                if (test.includes('resolveSharedRefs')) {
+                    options.filters = [jsonSchemaToOpenApiSchema];
+                }
             }
             catch (ex) {}
 
@@ -41,44 +54,36 @@ tests.forEach((test) => {
 });
 });
 
-const resolveSharedRefsTests = fs.readdirSync(path.join(__dirname,'resolveSharedRefs')).filter(file => {
-    return fs.statSync(path.join(__dirname, 'resolveSharedRefs', file)).isDirectory() && file !== 'include';
-});
+// const resolveSharedRefsTests = fs.readdirSync(path.join(__dirname,'resolveSharedRefs')).filter(file => {
+//     return fs.statSync(path.join(__dirname, 'resolveSharedRefs', file)).isDirectory() && file !== 'include';
+// });
 
 
-sinon.stub(crypto, 'createHash').callsFake(() => {
-    return {
-        update: sinon.stub().callsFake(() => {
-            return {
-                digest: sinon.stub().returns('123')
-            };
-        })
-    };
-});
 
-describe('Resolver tests, resolveSharedRefs option true', () => {
-resolveSharedRefsTests.forEach((test) => {
-    describe(test, () => {
-        it('should match expected output, resolveSharedRefs option true', (done) => {
-            const inputSpec = path.join(__dirname, 'resolveSharedRefs', test, 'input.yaml');
-            const input = yaml.parse(fs.readFileSync(inputSpec,'utf8'),{schema:'core'});
-            const output = yaml.parse(fs.readFileSync(path.join(__dirname, 'resolveSharedRefs', test, 'output.yaml'),'utf8'),{schema:'core'});
 
-            let options = { resolve: true, preserveMiro: false, source: inputSpec, filters: [jsonSchemaToOpenApiSchema], resolveSharedRefs: true };
-            try {
-                options = Object.assign({},options,yaml.parse(fs.readFileSync(path.join(__dirname, 'resolveSharedRefs', test, 'options.yaml'),'utf8'),{schema:'core'}));
-            }
-            catch (ex) {}
+// describe('Resolver tests, resolveSharedRefs option true', () => {
+// resolveSharedRefsTests.forEach((test) => {
+//     describe(test, () => {
+//         it('should match expected output, resolveSharedRefs option true', (done) => {
+//             const inputSpec = path.join(__dirname, 'resolveSharedRefs', test, 'input.yaml');
+//             const input = yaml.parse(fs.readFileSync(inputSpec,'utf8'),{schema:'core'});
+//             const output = yaml.parse(fs.readFileSync(path.join(__dirname, 'resolveSharedRefs', test, 'output.yaml'),'utf8'),{schema:'core'});
 
-            resolver.resolve(input, options.source, options)
-            .then(function(result){
-                assert.deepStrictEqual(result.openapi, output);
-                return done();
-            })
-            .catch(function(err){
-                return done(err);
-            });
-        });
-    });
-});
-});
+//             let options = { resolve: true, preserveMiro: false, source: inputSpec, filters: [jsonSchemaToOpenApiSchema], resolveSharedRefs: true };
+//             try {
+//                 options = Object.assign({},options,yaml.parse(fs.readFileSync(path.join(__dirname, 'resolveSharedRefs', test, 'options.yaml'),'utf8'),{schema:'core'}));
+//             }
+//             catch (ex) {}
+
+//             resolver.resolve(input, options.source, options)
+//             .then(function(result){
+//                 assert.deepStrictEqual(result.openapi, output);
+//                 return done();
+//             })
+//             .catch(function(err){
+//                 return done(err);
+//             });
+//         });
+//     });
+// });
+// });
