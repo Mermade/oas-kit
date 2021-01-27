41cbd72e1d2ea81941849b8db4c4293ff4945981
Hossein Mohammadi <hosseinm.developer@gmail.com>
2021-01-27 20:24:41 +0330

    Create index.d.ts

---
+++ packages/swagger2openapi/index.d.ts
@@ -0,0 +1,12 @@
+declare function mock(...args: any[]): any;
+
+export {
+    S2OError: mock,
+    targetVersion: mock,
+    convert: mock,
+    convertObj: mock,
+    convertUrl: mock,
+    convertStr: mock,
+    convertFile: mock,
+    convertStream: mock
+};
