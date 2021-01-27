4048b30c94567e1e481344e37b19e34388984515
Hossein Mohammadi <hosseinm.developer@gmail.com>
2021-01-27 21:57:58 +0330

    Update index.d.ts

--- packages/swagger2openapi/index.d.ts
+++ packages/swagger2openapi/index.d.ts
@@ -1,12 +1,23 @@
 declare function mock(...args: any[]): any;
+declare function convertObj(swagger: any, options: any, callback: any): any;
+declare function convertUrl(url: any, options: any, callback: any): any;
+declare function convertStr(str: any, options: any, callback: any): any;
+declare function convertFile(filename: any, options: any, callback: any): any;
+declare function convertStream(readable: any, options: any, callback: any): any;
+class S2OError extends Error {
+  name: string;
+}
+
+declare const targetVersion: string;
+declare const convert: convertObj;
 
 export {
-    S2OError: mock,
-    targetVersion: mock,
-    convert: mock,
-    convertObj: mock,
-    convertUrl: mock,
-    convertStr: mock,
-    convertFile: mock,
-    convertStream: mock
+  S2OError,
+  targetVersion,
+  convert,
+  convertObj,
+  convertUrl,
+  convertStr,
+  convertFile,
+  convertStream,
 };
