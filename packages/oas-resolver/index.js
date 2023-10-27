Mermade/oas-kit

commit aabd716b42ebac8b75135db8575725ee66867850

fix: defer optional removal of x-miro to end of resolution steps, refs #152

packages/oas-resolver/index.js
@@ -97,7 +97,6 @@ function resolveAllFragment(obj, context, src, parentPath, base, options) {
     recurse(obj,{},function(obj,key,state){
         if (isRef(obj, key)) {
             if (typeof obj.$fixed !== 'undefined') delete obj.$fixed;
-            if (!options.preserveMiro) delete obj['x-miro'];
         }
     });
 
@@ -426,6 +425,11 @@ function loopReferences(options, res, rej) {
                                 options.openapi = deRef(options.openapi,options.original,{verbose:options.verbose-1});
                                 if (options.verbose>1) console.warn(common.colour.yellow+'Finished internal resolution!',common.colour.normal);
                             }
+                            recurse(options.openapi,{},function(obj,key,state){
+                                if (isRef(obj, key)) {
+                                    if (!options.preserveMiro) delete obj['x-miro'];
+                                }
+                            });
                             res(options);
                         }
                     }

commit e4920863778216c25e24037dd51c9e056694ad31

fix https://github.com/decentralized-identity/veramo/issues/1281
caused by commit aabd716b42ebac8b75135db8575725ee66867850 addressing issue #152

packages/oas-resolver/index.js
@@ -466,7 +466,7 @@ function loopReferences(options, res, rej) {
                                 options.openapi = deRef(options.openapi,options.original,{verbose:options.verbose-1});
                                 if (options.verbose>1) console.warn(common.colour.yellow+'Finished internal resolution!',common.colour.normal);
                             }
-                            recurse(options.openapi,{},function(obj,key,state){
+                            recurse(options.openapi,{identityDetection:true},function(obj,key,state){
                                 if (isRef(obj, key)) {
                                     if (!options.preserveMiro) delete obj['x-miro'];
                                 }